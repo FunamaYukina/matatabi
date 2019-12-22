f7a6a284d04aa43bc8523d55b7270d5aa1c12b76
FunamaYukina <be-with-you1228@i.softbank.jp>
2019-12-23 01:19:49 +0900

    [fix] Rubocopによる修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class HomeController < ApplicationController
   layout "top", only: :top
 
@@ -6,5 +8,4 @@ class HomeController < ApplicationController
 
   def index
   end
-
 end
