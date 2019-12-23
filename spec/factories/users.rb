acb6ee0769a8b45dba1a28d5770bde1a8ab12d68
FunamaYukina <be-with-you1228@i.softbank.jp>
2019-12-23 12:06:58 +0900

    [add] userのspecファイル追加、テスト追記

---
+++ spec/factories/users.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :user do
+    email { "example@test.com" }
+    name { "test_user_name" }
+    password { "test_password" }
+    password_confirmation { "test_password" }
+    user_type {false}
+  end
+
+  factory :another_user, class: "User" do
+    email { "another_example@test.com" }
+    name { "test_another_user_name" }
+    password { "test_another_password" }
+    password_confirmation { "test_another_password" }
+    user_type {true}
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,77 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe FactoryBot do
+    it "有効なファクトリを持つこと" do
+      expect(build(:user)).to be_valid
+    end
+  end
+
+  describe "validation" do
+    let(:user) { build(:user) }
+
+    context "新規ユーザーの登録に成功する場合" do
+      it "ユーザーネーム、メールアドレス、パスワードがあれば有効であること" do
+        user = User.new(email: "example@test.com",
+                        name: "test_user_name",
+                        password: "test_password",
+                        password_confirmation: "test_password")
+        expect(user).to be_valid
+      end
+    end
+
+    context "新規ユーザーの登録に失敗する場合" do
+      it "ユーザーネームが重複した場合、ユーザー登録に失敗すること" do
+        create(:user)
+        existing_user = build(:another_user, name: "test_user_name")
+        existing_user.valid?
+        expect(existing_user.errors.full_messages).to include "ユーザーネーム は既に使用されています。"
+      end
+
+      it "emailが重複した場合、ユーザー登録に失敗すること" do
+        create(:user)
+        existing_user = build(:another_user, email: "example@test.com")
+        existing_user.valid?
+        expect(existing_user.errors.full_messages).to include "メールアドレス は既に使用されています。"
+      end
+
+      it "ユーザーネームがない場合、ユーザー登録に失敗すること" do
+        user.name = ""
+        user.valid?
+        expect(user.errors.full_messages).to include "ユーザーネーム が入力されていません。"
+      end
+
+      it "メールアドレスがない場合、ユーザー登録に失敗すること" do
+        user.email = ""
+        user.valid?
+        expect(user.errors.full_messages).to include "メールアドレス が入力されていません。"
+      end
+
+      it "パスワードがない場合、ユーザー登録に失敗すること" do
+        test_user = described_class.new(name: "test", email: "example@test.com")
+        test_user.valid?
+        expect(test_user.errors.full_messages).to include "パスワード が入力されていません。"
+      end
+
+      it "メールアドレスのフォーマットが正しくない場合、ユーザー登録に失敗すること" do
+        user.email = "exampletestcom"
+        user.valid?
+        expect(user.errors.full_messages).to include "メールアドレス は有効でありません。"
+      end
+
+      it "パスワードが６文字未満の場合、ユーザー登録に失敗すること" do
+        user.password = "pass"
+        user.valid?
+        expect(user.errors.full_messages).to include "パスワード は6文字以上に設定して下さい。"
+      end
+
+      it "パスワードと確認用パスワードが一致しない場合、ユーザー登録に失敗すること" do
+        user.password = "password"
+        user.password_confirmation = "pass"
+        user.valid?
+        expect(user.errors.full_messages).to include "確認用パスワード が一致していません。"
+      end
+    end
+  end
 end
