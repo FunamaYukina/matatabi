31d20566a1f8004ddfd5876f3e3dfd95b81ee7d8
FunamaYukina <be-with-you1228@i.softbank.jp>
2019-12-23 12:26:05 +0900

    [add] userのvalidation追記、インデント揃え

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20191222162557) do
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
