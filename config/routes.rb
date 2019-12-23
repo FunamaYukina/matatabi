5678bbd800e1ee974928e7ff6f5d4a1bbde55c32
FunamaYukina <be-with-you1228@i.softbank.jp>
2019-12-23 13:17:42 +0900

    [fix] app/views/userのフォルダ名をdeviseに変更,ログイン画面とログアウト機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get "top" => "home#index"
 
   devise_for :users, :controllers => {
-      :registrations => 'users/registrations'
+      :registrations => 'devise/registrations'
   }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20191222162557) do
 
-  create_table "users", force: :cascade do |t|
+  create_table "devise", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
