93f8ba858c7de1c5b29b5dbaf3d2bacba88a46f5
FunamaYukina <be-with-you1228@i.softbank.jp>
2019-12-25 22:28:53 +0900

    [add] edit画面デザイン完成、テスト整理、alertのデザイン修正

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -9,5 +9,18 @@ class ProfilesController < ApplicationController
   end
 
   def update
+    if current_user.profile.update(profile_params)
+      flash[:success] = "ユーザー情報を編集しました"
+      redirect_to user_profile_path
+    else
+      flash.now[:danger] = "ユーザー情報の更新に失敗しました"
+      render "settings/profiles/edit"
+    end
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:introduction_title, :introduction, :place,:image_file)
   end
 end
