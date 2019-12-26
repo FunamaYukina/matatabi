76e9a6db7d24c11dc567516e7ea3a999d791dbab
FunamaYukina <be-with-you1228@i.softbank.jp>
2019-12-27 01:14:22 +0900

    [add] planとadvisor関係追記、viewのデザイン追加、Planのedit,createできるように

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -3,14 +3,40 @@ class PlansController < ApplicationController
   end
 
   def new
+    @plan=current_user.advisor.plans.new
   end
 
   def create
+    @plan=current_user.advisor.plans.build(plan_params)
+    if @plan.save
+      flash[:success] = "プラン作成に成功しました"
+    else
+      flash[:danger] = "プラン作成に失敗しました"
+      render "/users/:user_name/plans/new"
+    end
+
+    redirect_to user_profile_path(user_name: current_user.name)
   end
 
   def edit
+    @plan = current_user.advisor.plans.find_by(id: params[:id])
   end
 
   def update
+    @plan = current_user.advisor.plans.find_by(id: params[:id])
+    if @plan.update(plan_params)
+      flash[:success] = "プラン情報を編集しました"
+      redirect_to user_profile_path(user_name: current_user.name)
+    else
+      flash.now[:danger] = "プラン情報の更新に失敗しました"
+      render "users/:user_name/plans/:id/edit"
+    end
+  end
+
+
+  private
+
+  def plan_params
+    params.require(:plan).permit(:id, :title, :description, :fee)
   end
 end
--- app/models/advisor.rb
+++ app/models/advisor.rb
@@ -2,4 +2,6 @@
 
 class Advisor < ApplicationRecord
   belongs_to :user
+  has_many :plans, dependent: :destroy
+  accepts_nested_attributes_for :plans
 end
